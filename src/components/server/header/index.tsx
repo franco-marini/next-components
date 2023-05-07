aa134acb72420c273cc220485931650cd3df895b
Franco Marini <franco.marini@radiumrocket.com>
2023-05-07 18:47:28 -0300

    Header component

--- src/components/server/header/index.tsx
+++ src/components/server/header/index.tsx
@@ -1,37 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Header = () => (
-  <header>
-    <div className="wrapper">
-      <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
-      </div>
-      <div>
-        <>
-          <span className="welcome">
-            Welcome, <b>Bob</b>!
-          </span>
-          {/* <Button size="small" label="Log out" onClick={() => console.log('Logged out')} /> */}
-        </>
-      </div>
-    </div>
-  </header>
-)
+import { SHeader, SLogo } from './styles'
+
+function Header() {
+  return (
+    <SHeader>
+      <SLogo>
+        <Image src="/images/logo.png" alt="logo image" width={150} height={75} />
+      </SLogo>
+      <span>
+        Welcome, <b>Pilot</b>!
+      </span>
+    </SHeader>
+  )
+}
 
 export default Header
---
+++ src/components/server/header/styles.ts
@@ -0,0 +1,21 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const SHeader = styled.header`
+  top: 0;
+  position: sticky;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0px 6px 10px 1px #0f0f0f1a;
+  background-color: #111111;
+`
+
+export const SLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`
