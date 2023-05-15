b68e730346050bfa6df50003241fc0af7efb5adf
Franco Marini <franco.marini@radiumrocket.com>
2023-05-15 15:28:08 -0300

    Update background when change theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react'
 
+import { addons } from '@storybook/addons'
+import { UPDATE_GLOBALS } from '@storybook/core-events'
 import { withThemeFromJSXProvider } from '@storybook/addon-styling'
 import { ThemeProvider } from 'styled-components'
 
@@ -26,7 +28,38 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      values: [
+        { name: 'light', value: '#FAFAFA' },
+        { name: 'dark', value: '#333333' },
+      ],
+    },
   },
 }
 
+const channel = addons.getChannel()
+
+const themeListener = (args) => {
+  if (args.globals.theme) {
+    let colorTheme = args.globals.theme
+    channel.removeListener(UPDATE_GLOBALS, themeListener)
+    channel.emit(UPDATE_GLOBALS, {
+      globals: {
+        theme: colorTheme,
+        backgrounds:
+          colorTheme === 'dark'
+            ? { name: 'dark', value: '#333333' }
+            : { name: 'light', value: '#FAFAFA' },
+      },
+    })
+    channel.addListener(UPDATE_GLOBALS, themeListener)
+  }
+}
+
+function setupBackgroundListener() {
+  channel.addListener(UPDATE_GLOBALS, themeListener)
+}
+
 export default preview
+
+setupBackgroundListener()
